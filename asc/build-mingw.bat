9539813b14df91db9e5827ad7cf02f77b3e28ff9
Gregor Klinke <gck@eyestep.org>
2003-04-13 23:39:41 +0000

    feedback output

--- asc/build-mingw.bat
+++ asc/build-mingw.bat
@@ -11,8 +11,10 @@ echo #define IMPLINIT "%ASC_HOME%/asc-init.scm" >> scmenv.h
 echo #define INIT_FILE_NAME "asc-init.scm" >> scmenv.h
 
 rem Compile C source files
+echo Compiling asc source files ...
 gcc -O -c ioext.c continue.c scm.c scmmain.c findexec.c script.c time.c repl.c scl.c eval.c sys.c subr.c debug.c unif.c rope.c
 
 rem Link C object files
+echo Linking asc ...
 gcc -o asc ioext.o continue.o scm.o scmmain.o findexec.o script.o time.o repl.o scl.o eval.o sys.o subr.o debug.o unif.o rope.o -lm
 
--- asc/build-unix.sh
+++ asc/build-unix.sh
@@ -9,8 +9,10 @@ echo "#define IMPLINIT \"$ASC_HOME/asc-init.scm\"" >> scmenv.h
 echo "#define INIT_FILE_NAME \"asc-init.scm\"" >> scmenv.h
 
 # Compile C source files
+echo "Compiling asc source files ..."
 cc -O -c ioext.c continue.c scm.c scmmain.c findexec.c script.c time.c repl.c scl.c eval.c sys.c subr.c debug.c unif.c rope.c
 
 # Link C object files
+echo "Linking asc ..."
 cc -o asc ioext.o continue.o scm.o scmmain.o findexec.o script.o time.o repl.o scl.o eval.o sys.o subr.o debug.o unif.o rope.o -lm -lc
 
