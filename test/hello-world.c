e3cbdff0aa330eb1cddc43fcd479f10cb68a2db7
Gregor Klinke <gck@eyestep.org>
2003-04-15 00:01:28 +0000

    added version

--- test/hello-world.c
+++ test/hello-world.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 #include "shared.h"
-
+#include "version.h"
 
 int main (int argc, char **argv)
 {
+  printf ("test file vr. %s\n", VERSION);
+
   show_hello_world();
 
   return 0;
