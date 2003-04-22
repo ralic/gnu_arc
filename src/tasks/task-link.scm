c220fa8349181056cd9d430d34d93a961780927d
Gregor Klinke <gck@eyestep.org>
2003-04-22 23:42:16 +0000

    support for local shared-linked exutables; rpath support

--- src/tasks/task-link.scm
+++ src/tasks/task-link.scm
@@ -15,7 +15,7 @@
 ;;  License along with this library; if not, write to the Free Software
 ;;  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-;; $Id: task-link.scm,v 1.3 2003/04/19 01:08:38 eyestep Exp $
+;; $Id: task-link.scm,v 1.4 2003/04/22 23:42:16 eyestep Exp $
 
 (arc:provide 'task-link)
 
@@ -25,39 +25,46 @@
 ;; Creates an application/executable by linking object files and libraries.
 ;; Only compatible object files may be linked.
 ;;
-;; :files STRING-LIST
+;; files: STRING-LIST
 ;; a list of object files to link into the executable
 ;;
-;; :libs STRING-LIST
+;; libs: STRING-LIST
 ;; A list of libraries to link with the executable.  The entries has to
 ;; give the "base library name" only, so for e.g.  for "libAbcX.so" give
 ;; "AbcX".  This task looks in the places given by ":libdirs" for libraries
 ;; and shared objects
 ;;
-;; :shared? BOOLEAN
+;; shared?: BOOLEAN
 ;; if #t, use shared libraries for the executable.  If #f, try to avoid
 ;; linking against shared libraries, but build a static application.
 ;;
-;; :appnm STRING
+;; appnm: STRING
 ;; the executable name to use.  This should not include probably necessary
 ;; name extensions, used to indicate executable files on some platforms.
 ;; 
-;; :appext STRING
+;; appext: STRING
 ;; the extension to use for the application.  This is only used if an
 ;; unusal executable extension should be used.
 
-;; :outdir STRING
+;; outdir: STRING
 ;; the directory to build the executable in
 ;;
-;; :libdirs STRING-LIST
+;; libdirs: STRING-LIST
 ;; A list of directories where to look for depending shared
 ;; objects/libraries.  Give only the directory itself (without any
 ;; additional option, e.g. without "-L" for gcc)
 ;;
-;; :nostdlib BOOLEAN
+;; nostdlib: BOOLEAN
 ;; if #t, don't use the standard system libraries and startup files when
 ;; linking.  Only the files you specify will be passed to the linker.
 ;; Default is #f
+;;
+;; rpath: STRING
+;; for shared links on elf systems: this adds an runtime directory to
+;; the executable, where the linker looks for shared libraries.  Note: this
+;; path is statically with the linked executable and therefore should only
+;; be used for local compilations/linkings (e.g. when a executable is to be
+;; linked with shared libraries but should not be installed)
 ;;
 ;; RETURNS
 ;; the name of the build application (string)
@@ -69,6 +76,8 @@
                             (appext string optional)
                             (outdir string optional)
                             (nostdlib? boolean optional)
+                            (rpath string optional)
+                            (local-exec-outdir string optional)
                             (libdirs strlist optional)) )
 (define (arc:link props body)
   (let* ((<backend> ((arc:handler-factory %arc:sysnm% 'task-link) 'alloc))
@@ -91,6 +100,15 @@
          (outdir (arc:aval 'outdir props #f))
          (nostdlib (arc:aval 'nostdlib? props #f))
          (libdirs (arc:aval 'libdirs props ())) 
+         (rpath (arc:aval 'rpath props #f))
+
+         (local-exec-outdir (if shared
+                                (arc:aval 'local-exec-outdir props #f)
+                                #f))
+         (local-exec-rpath (or rpath
+                               (if local-exec-outdir
+                                   libdirs
+                                   #f)))
          )
     (if (= (string-length appnm) 0)
         ;; (arc:log 'fatal "bad or empty application name"))
@@ -103,7 +121,13 @@
           #f)
         (let* ((fullnm (<backend> 'link-app outdir appnm appext
                                   libdirs autolibdirs shared nostdlib
-                                  files autolibs libs)))
+                                  files autolibs libs rpath))
+               (locexec (if (and shared
+                                 local-exec-outdir)
+                            (<backend> 'link-app local-exec-outdir appnm appext
+                                       libdirs autolibdirs shared nostdlib
+                                       files autolibs libs 
+                                       local-exec-rpath))))
           fullnm) )))
 
 
