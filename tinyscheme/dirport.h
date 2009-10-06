3b72a04c8079b512668bdb2f22f6d8c33c660001
eyestep <gck@eyestep.org>
2009-10-06 10:23:22 +0200

    Dropped tinyscheme interpreter

--- tinyscheme/dirport.h
+++
@@ -1,9 +0,0 @@
-#ifndef dirport_h
-#define dirport_h
-
-pointer dirport_is_dirport(scheme* sc, pointer args);
-pointer dirport_opendir(scheme *sc, pointer args);
-pointer dirport_readdir(scheme *sc, pointer args);
-pointer dirport_closedir(scheme *sc, pointer args);
-
-#endif
--- tinyscheme/scmenv.h
+++
@@ -1,3 +0,0 @@
-#define ARC_HOME "/usr/local/share/arc"
-#define IMPL_INIT "/usr/local/share/arc/asc-init.scm"
-#define INIT_FILE_NAME "asc-init.scm"
